e1c528f994bef25ddf85d32f847adc6fb8d5bca8
VoidXpulse <voidxpulse@gmail.com>
2019-10-07 00:11:28 -0400

    //second car class

--- Hendricks/src/Grades.java
+++ Hendricks/src/Grades.java
@@ -1,15 +1,44 @@
+import java.util.Scanner;
+
 public class Grades {
+    //Justin Ecarma
+    //9/15/2019
+    //Grades Lab
+    //Uses an object array to scale the program
     public static void main(String[] args) {
 
+        Scanner theScanner = new Scanner(System.in);
+        int numofs;//declaring variables
+        String name, average;
+        do {
+            System.out.println("How many students do you have?");
+            numofs = theScanner.nextInt();
+        }//gets size of array
+        while(numofs < 1);
 
+        Student obj[] = new Student[numofs];//making array
 
-        Student student1 = new Student("Mary");
-        Student student2 = new Student("Mike");
+        for(int i = 0; i < numofs; i++){
+            System.out.println("What is student " + (i + 1) + "'s name?");
+            name = theScanner.next();//getting name and input and making each object in the array
+            obj[i] = new Student(name);
+            obj[i].inputGrades();
+        }
 
-        student1.inputGrades();
-        System.out.println(student1.getName() + "'s average is " + student1.getAverage());
-
-        student2.inputGrades();
-        System.out.println(student2.getName() + "'s average is " + student2.getAverage());
+        for(int i = 0; i < numofs; i++){
+            if(i == 0){//printing out everything with the format
+                System.out.printf("%-10s |  ", "Name");
+                System.out.printf("%-10s |  ", "Test 1");
+                System.out.printf("%-10s |  ", "Test 2");
+                System.out.printf("%-10s", "Average");
+                System.out.println("");
+            }
+            for (int j = 0; j < 3; j++) {
+                System.out.printf("%-10s |  ", obj[i].toString(j));
+            }
+            average = obj[i].getAverage();
+            System.out.printf("%-10s  ", average);
+            System.out.println("");
+        }
     }
 }
--- Hendricks/src/Student.java
+++ Hendricks/src/Student.java
@@ -3,33 +3,41 @@ import java.util.Scanner;
 public class Student {
 
     private String name;
+    private String[] data = new String[3];
     Scanner theScanner = new Scanner(System.in);
-    private int test1, test2, average;
+    private int test1, test2, average;//making the variables
 
     public Student(String studentName){
+        //setting the name
         name = studentName;
     }
-    public void inputGrades(){
+    public void inputGrades(){//gets input for grades
         do{
-            System.out.println("Enter " + name + "'s score for Test One.");
+            System.out.println("Enter " + name + "'s score for Test One. Note: If this message repeats, your input was invalid.");
             test1 = theScanner.nextInt();
         }
         while(test1 < 0 || test1 > 100);
-
+        //error traps
         do{
-            System.out.println("Enter " + name + "'s score for Test Two.");
+            System.out.println("Enter " + name + "'s score for Test Two. Note: If this message repeats, your input was invalid.");
             test2 = theScanner.nextInt();
         }
         while(test2 < 0 || test2 > 100);
     }
-    public int getAverage() {
+    public String getAverage() {
         average = (test1 + test2)/2;
-        return average;
+        return average + "";
     }
     public String getName() {
         return name;
     }
     public void printName(){
         System.out.println(name);
+    }
+    public String toString(int piece) {//return specific output for input.
+        data[0] = name + "";
+        data[1] = test1 + "";
+        data[2] = test2 + "";
+        return data[piece];
     }
 }
